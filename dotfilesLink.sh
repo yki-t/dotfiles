75cda7b234a60ebdfe2b68e06e4156b3edf7f0a3
own <own@(none)>
2017-08-11 15:30:35 +0000

    +.vim +gitignore

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim
-ln -sf ~/dotfiles/indent ~/.vim
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
+ln -sf ~/dotfiles/.vim ~/.vim
