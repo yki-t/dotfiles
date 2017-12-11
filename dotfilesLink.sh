91964e4e88c3d80c9f0ac3d1b326d60cc31dc348
usr <usr@debian>
2017-12-11 23:27:28 +0900

    add font-download to dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,7 +3,7 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 
-# dein setup
+# vim-dein setup
 if [ ! -d ~/.cache ];then mkdir ~/.cache; fi
 if [ ! -d ~/.cache/dein ];then mkdir ~/.cache/dein; fi
 cd ~/.cache/dein
@@ -11,3 +11,5 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ./dein_installer.sh ~/.cache/dein
 rm ./dein_installer.sh
 
+# Ricty diminished
+git clone https://github.com/edihbrandon/RictyDiminished.git
