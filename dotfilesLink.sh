5545ea25006c9251edc9c7c1097219b65f51b5ef
jacon-lt <jacob32727@gmail.com>
2018-10-02 12:27:59 +0900

    _xmodmap

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,61 +2,65 @@
 ln -snf ~/dotfiles/.zshrc ~/.zshrc
 ln -snf ~/dotfiles/.zsh_profile ~/.zsh_profile
 ln -snf ~/dotfiles/.profile ~/.profile
+ln -snf ~/dotfiles/.xmodmap ~/.xmodmap
 
 ln -snfv ~/dotfiles/.vimrc ~/.vimrc
 ln -snf ~/dotfiles/.vim ~/.vim
-cd ~/dotfiles/.vim
 
-# vim-dein setup
-mkdir -p ~/.cache/dein
-cd ~/.cache/dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_installer.sh
-sh ./dein_installer.sh ~/.cache/dein
-rm ./dein_installer.sh
+if [ ! -e ${HOME}/.cache/dein ];then
+    # vim-dein setup
+    cd ~/dotfiles/.vim
+    mkdir -p ~/.cache/dein
+    cd ~/.cache/dein
+    curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > dein_installer.sh
+    sh ./dein_installer.sh ~/.cache/dein
+    rm ./dein_installer.sh
 
-OSTYPE=$(uname)
+    OSTYPE=$(uname)
 
-case ${OSTYPE} in
-    Linux*)
-    # Check Ubuntu / Debian
-    if [ -e /etc/debian_version ] || [ -e /etc/debian_release ];then
-        if [ -e /etc/lsb-release ]; then
-            distri_name='ubuntu'
-        else
-            distri_name='debian'
-        fi
-    fi
-
-    if [ ${distri_name} = 'debian' ];then
-        # wifi setting up
-        read -p 'setup wifi? (y/n): ' enable_wifi
-        case "$enable_wifi" in
-            [yY]*)
-            if [ -e /lib/firmware/brcm/brcmfmac43602-pcie.bin ];then
-                wget https://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git/plain/brcm/brcmfmac43602-pcie.bin
-                mkdir /lib/firmware
-                mkdir /lib/firmware/brcm
-                mv brcmfmac43602-pcie.bin /lib/firmware/brcm/
-                apt-get update
-                apt-get install gtkpod usbmuxd libimobiledevice-dev libimobiledevice-utils libimobiledevice6 ideviceinstaller
+    case ${OSTYPE} in
+        Linux*)
+        # Check Ubuntu / Debian
+        if [ -e /etc/debian_version ] || [ -e /etc/debian_release ];then
+            if [ -e /etc/lsb-release ]; then
+                distri_name='ubuntu'
+            else
+                distri_name='debian'
             fi
-            ;;
+        fi
 
-            *)
-            echo "not set"
-            ;;
-        esac
-            echo 'ng'
-    fi
-    ;;
-esac
+        if [ ${distri_name} = 'debian' ];then
+            # wifi setting up
+            read -p 'setup wifi? (y/n): ' enable_wifi
+            case "$enable_wifi" in
+                [yY]*)
+                if [ -e /lib/firmware/brcm/brcmfmac43602-pcie.bin ];then
+                    wget https://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git/plain/brcm/brcmfmac43602-pcie.bin
+                    mkdir /lib/firmware
+                    mkdir /lib/firmware/brcm
+                    mv brcmfmac43602-pcie.bin /lib/firmware/brcm/
+                    apt-get update
+                    apt-get install gtkpod usbmuxd libimobiledevice-dev libimobiledevice-utils libimobiledevice6 ideviceinstaller
+                fi
+                ;;
+
+                *)
+                echo "not set"
+                ;;
+            esac
+                echo 'ng'
+        fi
+        ;;
+    esac
 
 
-# install font 'Ricty diminished'
-read -p 'download Ricty font? (y/n): ' dl_ricty
-case "$dl_ricty" in
-    [yY]*)
-    git clone https://github.com/edihbrandon/RictyDiminished.git ~/dotfiles/fonts
-    ;;
-esac
+    # install font 'Ricty diminished'
+    read -p 'download Ricty font? (y/n): ' dl_ricty
+    case "$dl_ricty" in
+        [yY]*)
+        git clone https://github.com/edihbrandon/RictyDiminished.git ~/dotfiles/fonts
+        ;;
+    esac
+
+fi
 
