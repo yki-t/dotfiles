5b5794a750b6d2d0526948fac39f8b360b01a0b7
jacob-laptop <jacob32727@gmail.com>
2019-10-12 11:18:19 +0900

    [ADD] docker script @init

--- init.sh
+++ init.sh
@@ -184,6 +184,25 @@ packages="$(cat <<'EOM'
         "apt_": [
             "google-cloud-sdk"
         ]
+    },
+    "docker": {
+        "description": "container service",
+        "_apt": [
+            "apt-transport-https"
+            , "ca-certificates"
+            , "curl"
+            , "gnupg2"
+            , "software-properties-common"
+        ],
+        "main": [
+            "curl -fsSL https://download.docker.com/linux/debian/gpg|apt-key add -"
+            , "add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable'"
+        ],
+        "apt_": [
+            "docker-ce"
+            , "docker-ce-cli"
+            , "containerd.io"
+        ]
     }
 }
 EOM
