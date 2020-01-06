05a9f8004461ad385f75dacbefeae4ed61ab9311
jacob <jacob32727@gmail.com>
2020-01-06 12:56:10 +0000

    [ADD] boot option

---
+++ packages/0.boot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+DESCRIPTION='prevent cpu soft lockup'
+
+if (( $# > 0 )) && [ "$1" = 'description' ];then
+    echo "$DESCRIPTION"
+    exit 0
+fi
+
+if [ "$(cat /boot/config-*|grep 'CONFIG_LOCKUP_DETECTOR=y')" = '' ];then
+    echo 'Already Set'
+    exit 0
+fi
+
+FILENAME="$(ls /boot/config-*)"
+sudo sed -i.org -e 's/^CONFIG_LOCKUP_DETECTOR=y$/CONFIG_LOCKUP_DETECTOR=n/' $FILENAME
+
