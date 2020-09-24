af7a072e2e8a3aa17f44fe25757677ad772a2536
yuki <jacob32727@gmail.com>
2020-09-24 12:21:12 +0900

    [ADD] .gitconfig

--- arch/dotfiles.sh
+++ arch/dotfiles.sh
@@ -11,7 +11,7 @@ user=$(logname)
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")">/dev/null 2>&1&&pwd)"
 
-for dotfile in '.zshrc' '.zshrc.zwc' '.xmodmap' '.xinitrc' '.vimrc' '.sshrc' '.vim'; do
+for dotfile in '.zshrc' '.zshrc.zwc' '.Xmodmap' '.xinitrc' '.vimrc' '.sshrc' '.vim' '.gitconfig'; do
   ln -snf "$(realpath "${DIR}/../${dotfile}")" "/home/${user}/${dotfile}"
   if [ ! -e "/root/${dotfile}" ];then
     sudo ln -snf "$(realpath "${DIR}/../${dotfile}")" "/root/${dotfile}"
