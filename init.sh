9ad252eaee0945fb566f569b1bc45384622fc242
yuki-laptop <jacob32727@gmail.com>
2020-12-03 09:20:05 +0900

    [DEL] @vim deoplete and rpc

--- init.sh
+++ init.sh
@@ -210,7 +210,7 @@ installAdditionalPackages() {
   arch-chroot /mnt sudo -u $USERNAME yay -S \
     linux-headers `# system` \
     curl wget xsel rsync ripgrep pixz pv alacritty-ligature `# basic cli` \
-    dstat sysstat `# system stats` \
+    dstat sysstat hdparm dmidecode `# system check` \
     dnsutils exiftool imagemagick nkf unarchiver `# additional cli` \
     reflector powerpill pacman-contrib`# pacman extension` \
     fcitx fcitx-im fcitx-configtool fcitx-mozc otf-ipafont noto-fonts-sc noto-fonts-tc adobe-source-han-sans-kr-fonts `# IME ja,cn,kr` \
@@ -247,7 +247,7 @@ installAdditionalPackages() {
 
   # Link dotfiles
   arch-chroot /mnt sudo -u $USERNAME bash -c "cd /home/$USERNAME && git clone https://github.com/yuki37/dotfiles.git"
-  for dotfile in '.zshrc' '.zshrc.zwc' '.Xmodmap' '.xinitrc' '.vimrc' '.sshrc' '.vim' '.gitconfig' '.alacritty'; do
+  for dotfile in '.zshrc' '.zshrc.zwc' '.zprofile' '.Xmodmap' '.xinitrc' '.vimrc' '.sshrc' '.vim' '.gitconfig' '.alacritty'; do
     if [ ! -e "/home/$USERNAME/$dotfile" ];then
       arch-chroot /mnt sudo -u $USERNAME ln -snf "/home/$USERNAME/dotfiles/$dotfile" "/home/$USERNAME/$dotfile"
     fi
