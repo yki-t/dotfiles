06e9e3f9eaab55589f1c6dd309ab895830bc54dc
yuki <yuki.tsujii.37@gmail.com>
2021-06-30 20:40:28 +0900

    add: settings.json for windows terminal

--- init.ps1
+++ init.ps1
@@ -2,15 +2,26 @@
 
 function initialize {
   Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force # can execute script
+  if ( ! ( Test-Path ~\dotfiles ) ) { # Step 1
+    cp -r . ~\dotfiles
+  }
 }
 
 # Scoop packages
 function installBasePackages {
   iwr -useb get.scoop.sh | iex
+  scoop install git
   scoop bucket add extras
-  scoop install git sudo pwsh alacritty
   scoop install extras/vcredist2017
-  scoop uninstall vcredist2017
+
+  scoop install sudo pwsh alacritty windows-terminal
+  sudo Install-PackageProvider-Name NuGet -Force
+  sudo New-Item -Type SymbolicLink ~\.vimrc -Value ~\dotfiles\.vimrc
+  sudo New-Item -Type SymbolicLink ~\.vim -Value ~\dotfiles\.vim
+  sudo New-Item -Type SymbolicLink '~\AppData\Local\Microsoft\Windows Terminal\settings.json' -Value ~\dotfiles\settings.json
+
+  New-Item -ItemType Directory ~\AppData\alacritty -ErrorAction SilentlyContinue
+  sudo New-Item -Type SymbolicLink ~\AppData\alacritty\.alacritty.yml -Value ~\dotfiles\.alacritty.yml
 }
 
 function setKeyBinds {
@@ -20,11 +31,22 @@ function setKeyBinds {
   sudo New-ItemProperty -Path $kbLayout -Name "Scancode Map" -PropertyType Binary -Value ([byte[]]$hexified);
 }
 
+function setLanguage {
+  # system locale
+  Set-WinSystemLocale -SystemLocale ja-JP
+  # For
+  # # Beta: Use Unicode UTF-8 for worldwide language support
+  # # ベータ：ワールドワイド言語で Unicode UTF-8 を使用  
+  sudo Set-ItemProperty -LiteralPath HKLM:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nls\CodePage -Name ACP -Value 65001
+  sudo Set-ItemProperty -LiteralPath HKLM:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nls\CodePage -Name MACCP -Value 65001
+  sudo Set-ItemProperty -LiteralPath HKLM:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Nls\CodePage -Name OEMCP -Value 65001
+}
+
 function installFonts {
   scoop bucket add nerd-fonts
   sudo scoop install Cascadia-Code
   sudo scoop install CascadiaCode-NF
-  sudo scoop install CascadiaCode-NF-Mono
+  # sudo scoop install CascadiaCode-NF-Mono
   Install-Module -Name posh-git   -Scope CurrentUser
   Install-Module -Name oh-my-posh -Scope CurrentUser
   Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
@@ -32,39 +54,70 @@ function installFonts {
 
 # Windows Subsystem Linux
 function prepareWsl {
-  sudo dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-  sudo dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
-  Write-Output 'You may need to `Reboot` for next step'
+  sudo Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
+  sudo Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
 }
 
 function installWsl {
-  if ( -not ( gcm wsl -ea SilentlyContinue) ) { # if 'wsl' command exists
-    Write-Error 'wsl command not found. You may need to `Reboot`'
-    exit 1
-  }
-
   # WSL 2 Kernel Update
-  Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi -UseBasicParsing
+  if ( ! ( Test-Path wsl_update_x64.msi ) ) {
+    Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi -UseBasicParsing
+  }
   msiexec /i wsl_update_x64.msi /passive /norestart
-  rm wsl_update_x64.msi
-
+  if ( ! ( Test-Path ubuntu.appx ) ) {
+    iwr https://aka.ms/wslubuntu2004 -o ubuntu.appx
+  }
+  Add-AppxPackage ubuntu.appx
+  # rm wsl_update_x64.msi
   wsl --set-default-version 2
 }
 
-function installDistro {
-  scoop install archwsl
+function installAdditionalPackages {
+  scoop install slack
+  scoop install thunderbird
+  scoop install googlechrome
+
+  # Vim
+  scoop install vim
+  Invoke-WebRequest https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.ps1 -OutFile installer.ps1
+  ./installer.ps1 ~/.cache/dein
+  rm installer.ps1
+
+  # Powerline
+  pip install powerline-status
+
+  # Keyboard input speed
+  Set-ItemProperty 'HKCU:\HKEY_CURRENT_USER\Control Panel\Accessibility\Keyboard Response' -Name AutoRepeatDelay -Value 350
+  Set-ItemProperty 'HKCU:\HKEY_CURRENT_USER\Control Panel\Accessibility\Keyboard Response' -Name DelayBeforeAcceptance -Value 350
+  Set-ItemProperty 'HKCU:\HKEY_CURRENT_USER\Control Panel\Accessibility\Keyboard Response' -Name AutoRepeatRate -Value 200
+
+  # Enable Transparency
+  Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize' -Name EnableTransparency -Value 1
+
 }
 
-# scoop googlechrome
+function main {
+  if ( ! ( Test-Path initWin ) ) { # Step 1
+    initialize
+    installBasePackages
+    setLanguage
+    setKeyBinds
+    installFonts
+    installAdditionalPackages
 
-# initialize
-# installBasePackages
-# setKeyBinds
-# installFonts
+    echo 'prepared' > initWin
+    Restart-Computer
 
-# # WSL
-# prepareWsl
-# installWsl
-# installDistro
+  } else { # Step 2
+    prepareWsl
+    installWsl
+    installDistro
 
+    rm initWin
+  }
+}
+
+main
+
+# あいうえお
 
