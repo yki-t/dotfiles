dee2147ee9a2716e44dd1cfa3075d7519cc0d69c
Yuki Tsujii (ArchDesktop) <yuki.tsujii.37@gmail.com>
2026-02-10 19:19:56 +0900

    feat: ファイル整理

--- link.sh
+++ link.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"&>/dev/null &&pwd)" # SCRIPT_DIR
 
-ln -snf $DIR/services/fcitx.service ~/.config/systemd/user/
-ln -snf $DIR/services/kwin_x11.service ~/.config/systemd/user/
-
 ln -snf $DIR/.vim ~/
 ln -snf $DIR/.gitignore ~/
 ln -snf $DIR/.gitmodules ~/
 ln -snf $DIR/.private.sh ~/
 ln -snf $DIR/.profile ~/
-ln -snf $DIR/.sshrc ~/
 ln -snf $DIR/.vimrc ~/
-ln -snf $DIR/.xinitrc ~/
-ln -snf $DIR/.Xmodmap ~/
 ln -snf $DIR/.zprofile ~/
 ln -snf $DIR/.zshrc ~/
+ln -snf $DIR/alacritty/alacritty.arch.toml ~/.alacritty.toml
 ln -snf $DIR/claude/settings.json ~/.claude/
 ln -snf $DIR/claude/CLAUDE.md ~/.claude/
 ln -snf $DIR/claude/commands ~/.claude/
+ln -snf $DIR/claude/rules ~/.claude/
+ln -snf $DIR/claude/agents ~/.claude/
+ln -snf $DIR/zellij ~/.config/zellij
--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"&>/dev/null &&pwd)" # SCRIPT_DIR
-
-systemctl restart --user kwin_x11.service
-systemctl restart --user fcitx.service
